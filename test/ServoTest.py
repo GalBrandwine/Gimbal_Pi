1b725c437238eec8970ab6feb4145564cd9b19aa
GalBrandwine <gal080592@gmail.com>
2019-04-13 20:22:09 +0300

    updating test's

--- test/ServoTest.py
+++ test/ServoTest.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
-from driver.Raspi_PWM_Servo_Driver import PWM
 import time
+
+from driver.Raspi_PWM_Servo_Driver import PWM
 
 # ===========================================================================
 # Example Code
@@ -14,23 +15,22 @@ pwm = PWM(0x6F)
 servoMin = 150  # Min pulse length out of 4096
 servoMax = 600  # Max pulse length out of 4096
 
-def setServoPulse(channel, pulse):
-  pulseLength = 1000000                   # 1,000,000 us per second
-  pulseLength /= 60                       # 60 Hz
-  print ("%d us per period" % pulseLength)
-  pulseLength /= 4096                     # 12 bits of resolution
-  print ("%d us per bit" % pulseLength)
-  pulse *= 1000
-  pulse /= pulseLength
-  pwm.setPWM(channel, 0, pulse)
 
-pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
-while (True):
-  # Change speed of continuous servo on channel O
-  pwm.setPWM(0, 0, servoMin)
-  time.sleep(1)
-  pwm.setPWM(0, 0, servoMax)
-  time.sleep(1)
+def set_servo_pulse(channel, pulse):
+    pulse_length = 1000000  # 1,000,000 us per second
+    pulse_length /= 60  # 60 Hz
+    print("%d us per period" % pulse_length)
+    pulse_length /= 4096  # 12 bits of resolution
+    print("%d us per bit" % pulse_length)
+    pulse *= 1000
+    pulse /= pulse_length
+    pwm.set_pwm(channel, 0, pulse)
 
 
-
+pwm.set_pwm_freq(60)  # Set frequency to 60 Hz
+while True:
+    # Change speed of continuous servo on channel O
+    pwm.set_pwm(0, 0, servoMin)
+    time.sleep(1)
+    pwm.set_pwm(0, 0, servoMax)
+    time.sleep(1)
