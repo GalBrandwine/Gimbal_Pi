0557d85d0e9396148116c7079b2ada7ec6ddccd3
GalBrandwine <gal080592@gmail.com>
2019-04-27 21:03:57 +0300

    added SeroPi class from [](https://github.com/abelectronicsuk/ABElectronics_Python_Libraries/tree/master/ServoPi)
    and got it to work with thi project

--- test/ServoTest2.py
+++ test/ServoTest2.py
@@ -25,6 +25,9 @@ except ImportError:
             "Failed to import library from parent folder")
 
 
+yaw = 14
+
+
 def main():
     """
     Main program function
@@ -44,11 +47,11 @@ def main():
     # move the servo across its full range in increments of 10
     while True:
         for i in range(0, 250, 10):
-            servo.move(1, i)
+            servo.move(yaw, i)
             time.sleep(0.05)
 
         for i in range(250, 0, -10):
-            servo.move(1, i)
+            servo.move(yaw, i)
 
 
 if __name__ == "__main__":
