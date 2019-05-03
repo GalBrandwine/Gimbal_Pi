7f3b8e663b430e682f71d027880ade1ff44ada9d
GalBrandwine <gal080592@gmail.com>
2019-05-03 20:41:44 +0300

    added a pwm timer stop to code,

--- test/Servo_max_min_test.py
+++ test/Servo_max_min_test.py
@@ -43,8 +43,8 @@ def main():
 
     # set the servo minimum and maximum limits in milliseconds
     # the limits for a servo are typically between 1ms and 2ms.
-    servo.set_low_limit(0.8)
-    servo.set_high_limit(2.2)
+    servo.set_low_limit(0.6)
+    servo.set_high_limit(2.4)
 
     # Enable the outputs
     servo.output_enable()
