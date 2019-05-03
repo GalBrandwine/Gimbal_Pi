4eee00f476bc14caa645db9e66e8cad827af77b9
GalBrandwine <gal080592@gmail.com>
2019-05-03 20:12:56 +0300

    working a bitt

--- test/Servo_max_min_test.py
+++ test/Servo_max_min_test.py
@@ -50,14 +50,18 @@ def main():
     servo.output_enable()
 
     # move the servo across its full range in increments of 10
-    while True:
-        for i in range(0, 250, 10):
-            servo.move(yaw + 1, i)
-            time.sleep(0.05)
+    try:
+        while True:
+            for i in range(0, 250, 10):
+                servo.move(yaw + 1, i)
+                time.sleep(0.05)
 
-        for i in range(2, 0, -10):
-            servo.move(yaw + 1, i)
-        print("moving")
+            for i in range(2, 0, -10):
+                servo.move(yaw + 1, i)
+            print("moving")
+    except KeyboardInterrupt as err:
+        servo.output_disable()
+        print("output disabled")
 
 
 if __name__ == "__main__":
