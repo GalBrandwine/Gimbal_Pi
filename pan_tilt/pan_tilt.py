f2a49781908395830cb70b2d9b8f65757fdcc851
GalBrandwine <gal080592@gmail.com>
2019-05-04 13:32:59 +0300

    wrapping my servopi with pantilt

--- pan_tilt/pan_tilt.py
+++ pan_tilt/pan_tilt.py
@@ -118,11 +118,11 @@ def main():
         # print("servo pos: {}".format(servo.get_position(pitch + 1)))
         angle = 0
         while True:
-            servo.move(yaw + 1, 0,180)
+            servo.move(yaw + 1, 0)
             time.sleep(1)
 
-            for i in range(0, 180, 1):
-                servo.move(yaw + 1, i,180)
+            for i in range(0, 250, 1):
+                servo.move(yaw + 1, i)
                 print("servo pos: {}".format(servo.get_position(yaw + 1)))
                 time.sleep(.5)
 
